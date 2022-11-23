cda9dcc111386e2efbdcc8e2428160a1d9c21b46
jara <jaraguta03@gmail.com>
2022-11-23 12:09:53 +0300

    add requirement file

--- pdf.py
+++ pdf.py
@@ -5,9 +5,11 @@ inputs = sys.argv[1:]
 
 
 def pdf_combiner(pdf_list):
-    merger=PyPDF2.PdfMerger()
+    merger = PyPDF2.PdfMerger()
     for pdf in pdf_list:
         print(pdf)
         merger.append(pdf)
     merger.write('super.pdf')
+
+
 pdf_combiner(inputs)
